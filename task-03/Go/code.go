30801a9850927dd77c0ca12804afe2aa66e23707
KKSurendran06 <kksurendran95@gmail.com>
2023-08-30 13:50:56 +0530

    completed go

---
+++ task-03/Go/code.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func checkPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	var N int
+	fmt.Println("Enter a number")
+	fmt.Scanln(&N)
+
+	for i := 2; i <= N; i++ {
+		if checkPrime(i) {
+			fmt.Print(i, " ")
+		}
+	} 
+	fmt.Println("\n")
+}
