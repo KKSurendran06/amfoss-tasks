353a5393466c98047a1a0c4b5332fb2c113cfdd3
KKSurendran06 <kksurendran95@gmail.com>
2023-09-24 17:22:02 +0530

    corrected bot.py and scraper.py

--- task-06/scraper.py
+++ task-06/scraper.py
@@ -7,18 +7,19 @@ ESPN_URL = 'https://www.espncricinfo.com/live-cricket-score'
 def fetch_live_score():
     try:
         response = requests.get(ESPN_URL)
-        response.raise_for_status()  
+        response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        team1_name = soup.find_all(class_='ds-text-tight-m ds-font-bold ds-capitalize ds-truncate')[0].text.strip()
-        team2_name = soup.find_all(class_='ds-text-tight-m ds-font-bold ds-capitalize ds-truncate')[1].text.strip()
-        match_date = soup.find(class_='ds-text-tight-xs ds-truncate ds-text-typo-mid3').text.strip()
+        team1_name = soup.find_all(class_='ci-team-score ds-flex ds-justify-between ds-items-center ds-text-typo ds-my-1')[0].text.strip()
+        team2_name = soup.find_all(class_='ds-flex ds-items-center ds-min-w-0 ds-mr-1')[1].text.strip()
+        match_info = soup.find(class_='ds-text-tight-xs ds-truncate ds-text-typo-mid3').text.strip()
         match_status = soup.find(class_='ds-text-tight-s ds-font-regular ds-truncate ds-text-typo').text.strip()
-
-        live_score_info = f"**Match:** {team1_name} vs {team2_name}\n" \
-                          f"**Details:** {match_date}\n" \
-                          f"**Status:** {match_status}"
+        
+        live_score_info = f"**Team1:** {team1_name}\n" \
+                          f"**Team2:** {team2_name}\n"  \
+                          f"**Details:** {match_info}\n" \
+                          f"**Status:** {match_status}\n" \
 
         return live_score_info
     except Exception as e:
