10197ac6e44e1283784f13b68e702996d6a353cf
KKSurendran06 <kksurendran95@gmail.com>
2023-09-24 19:03:42 +0530

    updated

---
+++ task-07/README.md
@@ -0,0 +1,2 @@
+# Approach ->
+I began by creating a JSON file, learned basic JSON scripting through online resources, and found it surprisingly easy to work with. Then, I registered on OpenWeather to obtain an API key. Combining information from various resources and YouTube videos, I designed my extension's layout with HTML. I used JavaScript to fetch data from the API and added basic styling using CSS.
--- task-07/popup.js
+++ task-07/popup.js
@@ -4,18 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const weatherResult = document.getElementById('weatherResult');
     
     getWeatherButton.addEventListener('click', function() {
-        const city = cityInput.value;
-        if (city.trim() === '') {
-            alert('Please enter a city name.');
+        const city = cityInput.value.trim();
+        
+        if (city === '') {
+   
+            weatherResult.textContent = 'Please enter a city name.';
             return;
         }
-        
         
         const apiKey = '5faf06aa987d03f35face96a53edb0ac';
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         fetch(apiUrl)
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => {
                 const temperature = data.main.temp;
                 const description = data.weather[0].description;
@@ -24,7 +30,9 @@ document.addEventListener('DOMContentLoaded', function() {
             })
             .catch(error => {
                 console.error('Error fetching weather data:', error);
+    
                 weatherResult.textContent = 'An error occurred while fetching weather data.';
             });
     });
 });
+
